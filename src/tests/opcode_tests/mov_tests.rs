ed65358c92ce2205738e12324b78349195bac8aa
Drumato <drumatech109@gmail.com>
2020-06-18 16:25:14 +0900

    add some test cases

--- src/tests/opcode_tests/mov_tests.rs
+++ src/tests/opcode_tests/mov_tests.rs
@@ -1,31 +1,73 @@
+use crate::*;
+
+#[allow(dead_code)]
+const MOVRM8R8_CASES: [Instruction; 2] = [
+    Instruction {
+        opcode: Opcode::MOVRM8R8 {
+            rm8: Operand::GENERALREGISTER(GeneralPurposeRegister::BH),
+            r8: Operand::GENERALREGISTER(GeneralPurposeRegister::AH),
+        },
+    },
+    Instruction {
+        opcode: Opcode::MOVRM8R8 {
+            rm8: Operand::ADDRESSING {
+                base_reg: GeneralPurposeRegister::AL,
+                index_reg: None,
+                displacement: None,
+                scale: None,
+            },
+            r8: Operand::GENERALREGISTER(GeneralPurposeRegister::BH),
+        },
+    }
+];
+
+#[allow(dead_code)]
+const MOVRM64R64_CASES: [Instruction; 2] = [
+    Instruction {
+        opcode: Opcode::MOVRM64R64 {
+            rm64: Operand::GENERALREGISTER(GeneralPurposeRegister::RAX),
+            r64: Operand::GENERALREGISTER(GeneralPurposeRegister::RCX),
+        }
+    },
+    Instruction {
+        opcode: Opcode::MOVRM64R64 {
+            rm64: Operand::ADDRESSING {
+                base_reg: GeneralPurposeRegister::RAX,
+                index_reg: Some(GeneralPurposeRegister::RBX),
+                displacement: None,
+                scale: Some(0x4),
+            },
+            r64: Operand::GENERALREGISTER(GeneralPurposeRegister::RCX),
+        }
+    },
+];
+
+#[allow(dead_code)]
+const MOVRM64IMM32_CASES: [Instruction; 1] = [Instruction {
+    opcode: Opcode::MOVRM64IMM32 {
+        rm64: Operand::ADDRESSING {
+            base_reg: GeneralPurposeRegister::RAX,
+            index_reg: None,
+            displacement: None,
+            scale: None,
+        },
+        imm: Immediate::I32(60),
+    }
+}];
+
 #[cfg(test)]
 mod to_intel_tests {
-    use crate::*;
+    use super::*;
 
     #[test]
     fn movrm8r8_test() {
         // mov bh, ah
-        let inst = Instruction {
-            opcode: Opcode::MOVRM8R8 {
-                rm8: Operand::GENERALREGISTER(GeneralPurposeRegister::BH),
-                r8: Operand::GENERALREGISTER(GeneralPurposeRegister::AH),
-            }
-        };
+        let inst = &MOVRM8R8_CASES[0];
 
         assert_eq!(inst.to_intel_string(), "mov bh, ah");
 
         // mov BYTE PTR [rax], bh
-        let inst = Instruction {
-            opcode: Opcode::MOVRM8R8 {
-                rm8: Operand::ADDRESSING {
-                    base_reg: GeneralPurposeRegister::AL,
-                    index_reg: None,
-                    displacement: None,
-                    scale: None,
-                },
-                r8: Operand::GENERALREGISTER(GeneralPurposeRegister::BH),
-            }
-        };
+        let inst = &MOVRM8R8_CASES[1];
 
         assert_eq!(inst.to_intel_string(), "mov BYTE PTR [rax], bh");
     }
@@ -33,27 +75,12 @@ mod to_intel_tests {
     #[test]
     fn movrm64r64_test() {
         // mov rax, rcx
-        let inst = Instruction {
-            opcode: Opcode::MOVRM64R64 {
-                rm64: Operand::GENERALREGISTER(GeneralPurposeRegister::RAX),
-                r64: Operand::GENERALREGISTER(GeneralPurposeRegister::RCX),
-            }
-        };
+        let inst = &MOVRM64R64_CASES[0];
 
         assert_eq!(inst.to_intel_string(), "mov rax, rcx");
 
         // mov [rax + rbx * 4], rcx
-        let inst = Instruction {
-            opcode: Opcode::MOVRM64R64 {
-                rm64: Operand::ADDRESSING {
-                    base_reg: GeneralPurposeRegister::RAX,
-                    index_reg: Some(GeneralPurposeRegister::RBX),
-                    displacement: None,
-                    scale: Some(0x4),
-                },
-                r64: Operand::GENERALREGISTER(GeneralPurposeRegister::RCX),
-            }
-        };
+        let inst = &MOVRM64R64_CASES[1];
 
         assert_eq!(inst.to_intel_string(), "mov QWORD PTR [rax + rbx * 4], rcx");
     }
@@ -61,49 +88,24 @@ mod to_intel_tests {
     #[test]
     fn movrm64imm32_test() {
         // mov QWORD PTR [rax], 60
-        let inst = Instruction {
-            opcode: Opcode::MOVRM64IMM32 {
-                rm64: Operand::ADDRESSING {
-                    base_reg: GeneralPurposeRegister::RAX,
-                    index_reg: None,
-                    displacement: None,
-                    scale: None,
-                },
-                imm: Immediate::I32(60),
-            }
-        };
+        let inst = &MOVRM64IMM32_CASES[0];
         assert_eq!(inst.to_intel_string(), "mov QWORD PTR [rax], 60")
     }
 }
 
 #[cfg(test)]
 mod to_at_tests {
-    use crate::*;
+    use super::*;
 
     #[test]
     fn movrm8r8_test() {
         // mov bh, ah
-        let inst = Instruction {
-            opcode: Opcode::MOVRM8R8 {
-                rm8: Operand::GENERALREGISTER(GeneralPurposeRegister::BH),
-                r8: Operand::GENERALREGISTER(GeneralPurposeRegister::AH),
-            }
-        };
+        let inst = &MOVRM8R8_CASES[0];
 
         assert_eq!(inst.to_at_string(), "movb %ah, %bh");
 
         // mov BYTE PTR [rax], bh
-        let inst = Instruction {
-            opcode: Opcode::MOVRM8R8 {
-                rm8: Operand::ADDRESSING {
-                    base_reg: GeneralPurposeRegister::AL,
-                    index_reg: None,
-                    displacement: None,
-                    scale: None,
-                },
-                r8: Operand::GENERALREGISTER(GeneralPurposeRegister::BH),
-            }
-        };
+        let inst = &MOVRM8R8_CASES[1];
 
         assert_eq!(inst.to_at_string(), "movb %bh, (%rax)");
     }
@@ -111,27 +113,12 @@ mod to_at_tests {
     #[test]
     fn movrm64r64_test() {
         // mov rax, rcx
-        let inst = Instruction {
-            opcode: Opcode::MOVRM64R64 {
-                rm64: Operand::GENERALREGISTER(GeneralPurposeRegister::RAX),
-                r64: Operand::GENERALREGISTER(GeneralPurposeRegister::RCX),
-            }
-        };
+        let inst = &MOVRM64R64_CASES[0];
 
         assert_eq!(inst.to_at_string(), "movq %rcx, %rax");
 
         // mov [rax + rbx * 4], rcx
-        let inst = Instruction {
-            opcode: Opcode::MOVRM64R64 {
-                rm64: Operand::ADDRESSING {
-                    base_reg: GeneralPurposeRegister::RAX,
-                    index_reg: Some(GeneralPurposeRegister::RBX),
-                    displacement: None,
-                    scale: Some(0x4),
-                },
-                r64: Operand::GENERALREGISTER(GeneralPurposeRegister::RCX),
-            }
-        };
+        let inst = &MOVRM64R64_CASES[1];
 
         assert_eq!(inst.to_at_string(), "movq %rcx, (%rax, %rbx, 4)");
     }
@@ -139,49 +126,24 @@ mod to_at_tests {
     #[test]
     fn movrm64imm32_test() {
         // mov QWORD PTR [rax], 60
-        let inst = Instruction {
-            opcode: Opcode::MOVRM64IMM32 {
-                rm64: Operand::ADDRESSING {
-                    base_reg: GeneralPurposeRegister::RAX,
-                    index_reg: None,
-                    displacement: None,
-                    scale: None,
-                },
-                imm: Immediate::I32(60),
-            }
-        };
+        let inst = &MOVRM64IMM32_CASES[0];
         assert_eq!(inst.to_at_string(), "movq $60, (%rax)")
     }
 }
 
 #[cfg(test)]
 mod to_bytes_tests {
-    use crate::*;
+    use super::*;
 
     #[test]
     fn movrm8r8_test() {
         // mov bh, ah
-        let inst = Instruction {
-            opcode: Opcode::MOVRM8R8 {
-                rm8: Operand::GENERALREGISTER(GeneralPurposeRegister::BH),
-                r8: Operand::GENERALREGISTER(GeneralPurposeRegister::AH),
-            }
-        };
+        let inst = &MOVRM8R8_CASES[0];
 
         assert_eq!(inst.to_bytes(), vec![0x88, 0xe7]);
 
         // mov BYTE PTR [rax], bh
-        let inst = Instruction {
-            opcode: Opcode::MOVRM8R8 {
-                rm8: Operand::ADDRESSING {
-                    base_reg: GeneralPurposeRegister::AL,
-                    index_reg: None,
-                    displacement: None,
-                    scale: None,
-                },
-                r8: Operand::GENERALREGISTER(GeneralPurposeRegister::BH),
-            }
-        };
+        let inst = &MOVRM8R8_CASES[1];
 
         assert_eq!(inst.to_bytes(), vec![0x88, 0x38]);
     }
@@ -189,27 +151,12 @@ mod to_bytes_tests {
     #[test]
     fn movrm64r64_test() {
         // mov rax, rcx
-        let inst = Instruction {
-            opcode: Opcode::MOVRM64R64 {
-                rm64: Operand::GENERALREGISTER(GeneralPurposeRegister::RAX),
-                r64: Operand::GENERALREGISTER(GeneralPurposeRegister::RCX),
-            }
-        };
+        let inst = &MOVRM64R64_CASES[0];
 
         assert_eq!(inst.to_bytes(), vec![0x48, 0x89, 0xc8]);
 
         // mov [rax + rbx * 4], rcx
-        let inst = Instruction {
-            opcode: Opcode::MOVRM64R64 {
-                rm64: Operand::ADDRESSING {
-                    base_reg: GeneralPurposeRegister::RAX,
-                    index_reg: Some(GeneralPurposeRegister::RBX),
-                    displacement: None,
-                    scale: Some(0x4),
-                },
-                r64: Operand::GENERALREGISTER(GeneralPurposeRegister::RCX),
-            }
-        };
+        let inst = &MOVRM64R64_CASES[1];
 
         assert_eq!(inst.to_bytes(), vec![0x48, 0x89, 0x0c, 0x98]);
     }
@@ -217,17 +164,7 @@ mod to_bytes_tests {
     #[test]
     fn movrm64imm32_test() {
         // mov QWORD PTR [rax], 60
-        let inst = Instruction {
-            opcode: Opcode::MOVRM64IMM32 {
-                rm64: Operand::ADDRESSING {
-                    base_reg: GeneralPurposeRegister::RAX,
-                    index_reg: None,
-                    displacement: None,
-                    scale: None,
-                },
-                imm: Immediate::I32(60),
-            }
-        };
+        let inst = &MOVRM64IMM32_CASES[0];
         assert_eq!(inst.to_bytes(), vec![0x48, 0xc7, 0x00, 0x3c, 0x00, 0x00, 0x00])
     }
 }
--- src/tests/opcode_tests/push_tests.rs
+++ src/tests/opcode_tests/push_tests.rs
@@ -1,15 +1,137 @@
+use crate::*;
+
+#[allow(dead_code)]
+const PUSHIMM32_CASES: [Instruction; 1] = [
+    Instruction {
+        opcode: Opcode::PUSHIMM32 {
+            imm: Immediate::I32(60),
+        }
+    },
+];
+
+#[allow(dead_code)]
+const PUSHR64_CASES: [Instruction; 2] = [
+    Instruction {
+        opcode: Opcode::PUSHR64 {
+            r64: GeneralPurposeRegister::RAX,
+        }
+    },
+    Instruction {
+        opcode: Opcode::PUSHR64 {
+            r64: GeneralPurposeRegister::R15,
+        }
+    },
+];
+
+#[allow(dead_code)]
+const PUSHRM64_CASES: [Instruction; 2] = [
+    Instruction {
+        opcode: Opcode::PUSHRM64 {
+            rm64: Operand::ADDRESSING {
+                base_reg: GeneralPurposeRegister::RAX,
+                index_reg: None,
+                displacement: None,
+                scale: None,
+            },
+        }
+    },
+    Instruction {
+        opcode: Opcode::PUSHRM64 {
+            rm64: Operand::ADDRESSING {
+                base_reg: GeneralPurposeRegister::RAX,
+                index_reg: Some(GeneralPurposeRegister::RBX),
+                displacement: Some(Displacement::DISP8(-4)),
+                scale: Some(4),
+            },
+        }
+    }
+];
+
 #[cfg(test)]
-mod to_bytes_tests {
-    use crate::*;
+mod to_intel_tests {
+    use super::*;
 
     #[test]
     fn pushimm32_test() {
         // push 60
-        let inst = Instruction {
-            opcode: Opcode::PUSHIMM32 {
-                imm: Immediate::I32(60),
-            }
-        };
+        let inst = &PUSHIMM32_CASES[0];
+
+        assert_eq!(inst.to_intel_string(), "push 60");
+    }
+
+    #[test]
+    fn pushr64_test() {
+        // push rax
+        let inst = &PUSHR64_CASES[0];
+
+        assert_eq!(inst.to_intel_string(), "push rax");
+
+        // push r15
+        let inst = &PUSHR64_CASES[1];
+
+        assert_eq!(inst.to_intel_string(), "push r15");
+    }
+
+    #[test]
+    fn pushrm64_test() {
+        // push [rax]
+        let inst = &PUSHRM64_CASES[0];
+
+        assert_eq!(inst.to_intel_string(), "push QWORD PTR [rax]");
+
+        // push -4[rax + rbx * 4]
+        let inst = &PUSHRM64_CASES[1];
+
+        assert_eq!(inst.to_intel_string(), "push QWORD PTR -4[rax + rbx * 4]");
+    }
+}
+#[cfg(test)]
+mod to_at_tests {
+    use super::*;
+
+    #[test]
+    fn pushimm32_test() {
+        // push 60
+        let inst = &PUSHIMM32_CASES[0];
+
+        assert_eq!(inst.to_at_string(), "pushq $60");
+    }
+
+    #[test]
+    fn pushr64_test() {
+        // push rax
+        let inst = &PUSHR64_CASES[0];
+
+        assert_eq!(inst.to_at_string(), "pushq %rax");
+
+        // push r15
+        let inst = &PUSHR64_CASES[1];
+
+        assert_eq!(inst.to_at_string(), "pushq %r15");
+    }
+
+    #[test]
+    fn pushrm64_test() {
+        // push [rax]
+        let inst = &PUSHRM64_CASES[0];
+
+        assert_eq!(inst.to_at_string(), "pushq (%rax)");
+
+        // push -4[rax + rbx * 4]
+        let inst = &PUSHRM64_CASES[1];
+
+        assert_eq!(inst.to_at_string(), "pushq -4(%rax, %rbx, 4)");
+    }
+}
+
+#[cfg(test)]
+mod to_bytes_tests {
+    use super::*;
+
+    #[test]
+    fn pushimm32_test() {
+        // push 60
+        let inst = &PUSHIMM32_CASES[0];
 
         assert_eq!(inst.to_bytes(), vec![0x68, 0x3c, 0x00, 0x00, 0x00]);
     }
@@ -17,20 +139,12 @@ mod to_bytes_tests {
     #[test]
     fn pushr64_test() {
         // push rax
-        let inst = Instruction {
-            opcode: Opcode::PUSHR64 {
-                r64: GeneralPurposeRegister::RAX,
-            }
-        };
+        let inst = &PUSHR64_CASES[0];
 
         assert_eq!(inst.to_bytes(), vec![0x50]);
 
         // push r15
-        let inst = Instruction {
-            opcode: Opcode::PUSHR64 {
-                r64: GeneralPurposeRegister::R15,
-            }
-        };
+        let inst = &PUSHR64_CASES[1];
 
         assert_eq!(inst.to_bytes(), vec![0x41, 0x57]);
     }
@@ -38,30 +152,12 @@ mod to_bytes_tests {
     #[test]
     fn pushrm64_test() {
         // push [rax]
-        let inst = Instruction {
-            opcode: Opcode::PUSHRM64 {
-                rm64: Operand::ADDRESSING {
-                    base_reg: GeneralPurposeRegister::RAX,
-                    index_reg: None,
-                    displacement: None,
-                    scale: None,
-                },
-            }
-        };
+        let inst = &PUSHRM64_CASES[0];
 
         assert_eq!(inst.to_bytes(), vec![0xff, 0x30]);
 
         // push -4[rax + rbx * 4]
-        let inst = Instruction {
-            opcode: Opcode::PUSHRM64 {
-                rm64: Operand::ADDRESSING {
-                    base_reg: GeneralPurposeRegister::RAX,
-                    index_reg: Some(GeneralPurposeRegister::RBX),
-                    displacement: Some(Displacement::DISP8(-4)),
-                    scale: Some(4),
-                },
-            }
-        };
+        let inst = &PUSHRM64_CASES[1];
 
         assert_eq!(inst.to_bytes(), vec![0xff, 0x74, 0x98, 0xfc]);
     }
